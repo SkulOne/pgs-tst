38e0cdb7e6aea9dfbc4ef06bc808a6fd67fd4888
Sultan Kuliev <wndrngr13@gmail.com>
2026-04-15 23:46:10 +0400

    tst

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,7 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
+  some: string;
   protected readonly title = signal('simbirsoft-soccerstat');
+  constructor(){this.some="wasdadas"}
 }
