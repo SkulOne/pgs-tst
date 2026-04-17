9f3a479e29b1f674b965ab46e02d57795a794514
Sultan Kuliev <wndrngr13@gmail.com>
2026-04-17 17:34:09 +0400

    tst2

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Tst } from "./tst/tst";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Tst],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
